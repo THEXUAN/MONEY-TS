89822f8ba510cf9161338605f8eac25d7799c1ed
StevenLiang <skt1piglelt@gmail.com>
2020-11-12 05:27:14 +0800

    deploy

---
+++ precache-manifest.78baf6bbe882b61f1318e1798bdd3dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1f894e592d34f1905c",
+    "url": "/MONEY-TS/css/app.5427b069.css"
+  },
+  {
+    "revision": "0878872e4560864d4cca966332d9592a",
+    "url": "/MONEY-TS/index.html"
+  },
+  {
+    "revision": "2d1f894e592d34f1905c",
+    "url": "/MONEY-TS/js/app.9527d8d8.js"
+  },
+  {
+    "revision": "f1a23b84df89a2db1c0c",
+    "url": "/MONEY-TS/js/chunk-vendors.d7690047.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/MONEY-TS/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MONEY-TS/robots.txt"
+  }
+]);
